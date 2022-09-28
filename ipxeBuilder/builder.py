1fc17bfa02380d15d4cd4bd433cb2039fb4a26c5
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-28 14:50:21 +0700

    first

---
+++ ipxeBuilder/builder.py
@@ -0,0 +1,12 @@
+from . import Command
+
+class Builder:
+	commands=[]
+	def __init__(self):
+		pass
+
+	def add(self, command:Command):
+		self.commands.append(command)
+
+	def build(self):
+		return "#!ipxe\n"+"\n".join([i.code() for i in self.commands]) 
